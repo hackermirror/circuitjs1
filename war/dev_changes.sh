2d0a8bfbcac67221166a78c5d6ab85c4690e5fff
Raquel Horta Bartomeu <raquelhortab@gmail.com>
2021-08-01 11:36:45 +0200

    changes Dockerfile, added script to adapt the iframe-main window communication script url for DEV

---
+++ war/dev_changes.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+DEV_URL=${DEV_URL:-http://0.0.0.0:3000/electronics/js/iframe_comunication.js}
+DEV_SCRIPT="<script language=\"javascript\" src=\"$DEV_URL\"></script>"
+echo "Gonna check if development changes need to be made"
+if [ "$BUILD_ENV" = "development" ]; then
+  cd /usr/local/apache2
+  echo "We are in development, making some changes"
+  mkdir circuitjs1/war/dev
+  cp circuitjs1/war/circuitjs.html circuitjs1/war/dev/circuitjs.html
+  sed -i "252s|.*|$DEV_SCRIPT|" circuitjs1/war/dev/circuitjs.html
+  cp circuitjs1/war/dev/circuitjs.html circuitjs1/war/circuitjs.html
+  cp -r circuitjs1/war/. /usr/local/apache2/htdocs/
+  echo "Changed the communication script url, now being:"
+  awk 'NR==252' /usr/local/apache2/circuitjs1/war/circuitjs.html
+else
+  echo "We are in production so no changes where made"
+fi
+echo "Done."
